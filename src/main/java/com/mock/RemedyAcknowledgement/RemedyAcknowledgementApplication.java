b190845df824540369ff3715adb8e5e987b78967
Vijayan Raja shanmugakani <vijayan.r@hcl.com>
2020-03-03 11:41:46 +0530

    first commit

---
+++ src/main/java/com/mock/RemedyAcknowledgement/RemedyAcknowledgementApplication.java
@@ -0,0 +1,13 @@
+package com.mock.RemedyAcknowledgement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemedyAcknowledgementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemedyAcknowledgementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mock/RemedyAcknowledgement/RemedyAcknowledgementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mock.RemedyAcknowledgement;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemedyAcknowledgementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
